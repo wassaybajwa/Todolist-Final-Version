a99c352274094c05d9c7ec5a2ef20bcf10a811ec
Wassay Bajwa <wassaybajwa@hotmail.com>
2021-07-16 05:59:32 -0400

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://wassaybajwa:43.WKD.mUXavtL6@glyde.ehrwo.mongodb.net/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://wassaybajwa@glyde.ehrwo.mongodb.net/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
